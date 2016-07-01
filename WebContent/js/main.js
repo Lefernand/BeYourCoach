8697c16217dbf9b526cc7f4f886eaa93a866b739
Mathias-Rui Lopes <lopes.mathiasrui@gmail.com>
2016-07-01 19:06:54 +0200

    ClickOut js pour la box Welcome

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -10,3 +10,17 @@ $(function(){
 	});
 	
 });
+
+$(document).mouseup(function (e)
+		{
+		    var container = $(".helloGuy");
+
+		    if (!container.is(e.target) // if the target of the click isn't the container...
+		        && container.has(e.target).length === 0) // ... nor a descendant of the container
+		    {
+		    	container.fadeOut( "slow", function() {
+		    		container.hide();
+		    	  });
+		        
+		    }
+		});
